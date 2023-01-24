357a0e7917667bf521a319289482a1886408e5ec
Evgeniy Orlyanskiy <evgeniy.orlyanskiy@gmail.com>
2023-01-25 06:14:33 +1000

    init frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/sass/main.sass', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/sass/main.sass',
                 'resources/js/app.js',
             ],
             refresh: true,
