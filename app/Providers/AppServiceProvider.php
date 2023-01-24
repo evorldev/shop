8ac939b080baa6a037a97682c2eb6bf538ae65ba
Evgeniy Orlyanskiy <evgeniy.orlyanskiy@gmail.com>
2023-01-25 05:46:16 +1000

    init config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        // TODO replace with 500
+        DB::whenQueryingForLongerThan(5000, function (Connection $connection, QueryExecuted $event) {
+            // TODO
+        });
+
+        // TODO
     }
 }
