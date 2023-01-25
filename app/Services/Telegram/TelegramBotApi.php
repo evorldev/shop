21d7e27cf37d395a2c71442d44b39a65cd0a811c
Evgeniy Orlyanskiy <evgeniy.orlyanskiy@gmail.com>
2023-01-26 01:20:24 +1000

    refactoring (homework)

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,17 +2,32 @@
 
 namespace App\Services\Telegram;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 final class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $text): void
+    public static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text,
-        ]);
+        try {
+            $response = Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text,
+            ])->json();
+
+            if ($response['ok'] === true) {
+                return true;
+            }
+
+            throw new Exception($response['description'], $response['error_code']);
+        } catch (Exception $e) {
+            logger()
+                ->channel('single')
+                ->error('TelegramBotApi::sendMessage: [' . $e->getCode() . '] ' . $e->getMessage());
+
+            return false;
+        }
     }
 }
