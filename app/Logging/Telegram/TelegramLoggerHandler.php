aec25da28ab8af97f3a9a561f1af29a9fafcd4c9
Evgeniy Orlyanskiy <evgeniy.orlyanskiy@gmail.com>
2023-01-30 17:37:21 +1000

    refactoring

--- app/Logging/Telegram/TelegramLoggerHandler.php
+++ app/Logging/Telegram/TelegramLoggerHandler.php
@@ -4,7 +4,6 @@ namespace App\Logging\Telegram;
 
 use App\Services\Telegram\TelegramBotApi;
 use Monolog\Handler\AbstractProcessingHandler;
-use Monolog\Logger;
  
 class TelegramLoggerHandler extends AbstractProcessingHandler
 {
@@ -14,9 +13,7 @@ class TelegramLoggerHandler extends AbstractProcessingHandler
 
     public function __construct(array $config)
     {
-        $level = Logger::toMonologLevel($config['level']);
-
-        parent::__construct($level);
+        parent::__construct($config['level']);
 
         $this->token = $config['token'];
         $this->chatId = $config['chat_id'];
