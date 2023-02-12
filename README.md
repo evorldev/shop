1d3e20cb5291b86c78ad28b3a9fe9ab477438c1b
Evgeniy Orlyanskiy <evorl.dev@gmail.com>
2023-02-13 01:50:38 +1000

    refactoring

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Installation
 
- - php artisan shop:install
+ - make .env (!!! Important !!!)
 
+ - composer install
+ - npm install
+ - npm run build
+
+ - php artisan storage:link
+ - php artisan key:generate
+
+ - php artisan migrate
 
 # Deploy 
+
+ - make .env (!!! Important !!!)
+
+ - composer install --optimize-autoloader --no-dev
+
+ - php artisan storage:link
+ - php artisan key:generate
+
+ - php artisan migrate
+
+ - php artisan config:cache
+ - php artisan route:cache
+ - php artisan view:cache
--- app/Console/Commands/FreshCommand.php
+++ app/Console/Commands/FreshCommand.php
@@ -3,29 +3,56 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 
 class FreshCommand extends Command
 {
+    use ConfirmableTrait;
+
     protected $signature = 'shop:fresh';
 
     protected $description = 'Fresh DB with seed';
 
-
     public function handle(): int
     {
-        if (app()->isProduction()) {
+        if (! $this->confirmToProceed()) {
             return Command::FAILURE;
         }
 
-        Storage::disk('public')->deleteDirectory('images');
-        Storage::disk('public')->createDirectory('images/brands');
-        Storage::disk('public')->createDirectory('images/products');
+        $this->deleteDirectories();
 
-        $this->call('migrate:fresh', [
-            '--seed' => true,
-        ]);
+        Artisan::call(
+            'migrate:fresh',
+            [
+                '--force' => true,
+                '--seed' => true,
+            ],
+            $this->output
+        );
 
         return Command::SUCCESS;
+    }
+
+    private function deleteDirectories(): void
+    {
+        $directories = Storage::disk('public')->directories('images');
+
+        if (empty($directories)) {
+            $this->components->warn('Directories with images not found.');
+
+            return;
+        }
+
+        $this->components->info('Deleting directories with images.');
+
+        foreach ($directories as $directory) {
+            $this->components->task($directory, function () use ($directory) {
+                Storage::disk('public')->deleteDirectory($directory);
+            });
+        }
+
+        $this->newLine();
     }
 }
