b90a8bc27206d2b995620063883ec03e6add6c4f
Evgeniy Orlyanskiy <evorl.dev@gmail.com>
2023-02-28 17:36:05 +1000

    lesson 6-2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,30 @@ class ProductController extends Controller
 {
     public function __invoke(Product $product = null)
     {
+        $also = null;
+        try {
+            // session()->forget('also');
+            $_also = session('also', []);
+            $_also = array_diff($_also, [$product->id]);
+
+            $also = Product::query()
+                ->whereIn('id', $_also)
+                // ->where('id', '!=', $product->id)
+                ->limit(4)
+                ->get()
+                ->sortBy(function($item) use ($_also){
+                    return array_search($item->getKey(), $_also);
+                });
+
+            array_unshift($_also, $product->id);
+            $_also = array_slice($_also, 0, 5);
+            session()->put('also', $_also);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+
+
         $product->load(['optionValues.option']);
         $options = $product->optionValues->mapToGroups(function ($item) {
             return [$item->option->title => $item];
@@ -16,6 +40,7 @@ class ProductController extends Controller
         return view('product.show', compact(
             'product',
             'options',
+            'also',
         ));
     }
 }
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -115,10 +115,13 @@
     </section>
 
     <!-- Watched products  -->
-    <section class="mt-16 xl:mt-24">
-        <h2 class="mb-12 text-lg lg:text-[42px] font-black">Просмотренные товары</h2>
-        <!-- Products list -->
-        <div class="products grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-x-8 gap-y-8 lg:gap-y-10 2xl:gap-y-12">
-        </div>
-    </section>
+    @isset($also)
+        <section class="mt-16 xl:mt-24">
+            <h2 class="mb-12 text-lg lg:text-[42px] font-black">Просмотренные товары</h2>
+            <!-- Products list -->
+            <div class="products grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-x-8 gap-y-8 lg:gap-y-10 2xl:gap-y-12">
+                @each ('product.shared.card', $also, 'item')
+            </div>
+        </section>
+    @endisset
 @endsection
